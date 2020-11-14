2bdd5d4804e026abc5688d316c1c540156fe8cb2
neomat91 <rahulramesh1995@gmail.com>
2020-11-13 18:00:28 -0700

    added db model and able to retrieve data from database to view page

--- todoapp/models.py
+++ todoapp/models.py
@@ -4,3 +4,5 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class TodoItem(models.Model):
+	content = models.TextField()
--- todoapp/views.py
+++ todoapp/views.py
@@ -2,7 +2,10 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import TodoItem
 
 # Create your views here.
 def todoView(request):
-	return render(request, 'todo.html');
+	all_todo_items = TodoItem.objects.all()
+	return render(request, 'todo.html',
+		{'all_items': all_todo_items})
